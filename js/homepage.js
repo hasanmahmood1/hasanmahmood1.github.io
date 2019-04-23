6878a90b0826b944d7adb7d5bca5fa522d11b102
Sushovan Majhi <sushovan.majhi08@gmail.com>
2019-04-23 11:25:26 -0500

    add

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,11 @@
 $('document').ready(function( ){
     $('.ui.dropdown').dropdown( );
 
+    $('.slick').slick({
+	dots: true,
+	arrows: true
+    });
+    
     $(".menu .item")
 	.click(function() {
 	    $('html, body').animate({
